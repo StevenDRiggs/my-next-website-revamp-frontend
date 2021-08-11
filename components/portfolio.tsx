b804c80d6d0aa7e2cbdd8a0c878868e1d599e2a9
StevenDRiggs <sfriggs21@gmail.com>
2021-08-11 13:31:42 -0600

    hid fake portfolio items (kept for reference)

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -19,51 +19,6 @@ const Portfolio = () => {
             <img className={styles.youTubeIcon} src='/images/youtube_social_square_red.png' alt='See demo on YouTube' />
           </div>
         </div>
-        <div className={styles.portfolioItem}>
-          <h3>Project Title</h3>
-          <img src='https://picsum.photos/200?random=2' alt='Project Screenshot' />
-          <div className={styles.projectConnections}>
-            <img className={styles.faIcon} src='/images/connectdevelop-brands.svg' alt='See it live' />
-            <img className={styles.gitHubMark} src='/images/GitHub-Mark-64px.png' alt='See it on GitHub' />
-            <img className={styles.youTubeIcon} src='/images/youtube_social_square_red.png' alt='See demo on YouTube' />
-          </div>
-        </div>
-        <div className={styles.portfolioItem}>
-          <h3>Project Title</h3>
-          <img src='https://picsum.photos/200?random=3' alt='Project Screenshot' />
-          <div className={styles.projectConnections}>
-            <img className={styles.faIcon} src='/images/connectdevelop-brands.svg' alt='See it live' />
-            <img className={styles.gitHubMark} src='/images/GitHub-Mark-64px.png' alt='See it on GitHub' />
-            <img className={styles.youTubeIcon} src='/images/youtube_social_square_red.png' alt='See demo on YouTube' />
-          </div>
-        </div>
-        <div className={styles.portfolioItem}>
-          <h3>Project Title</h3>
-          <img src='https://picsum.photos/200?random=4' alt='Project Screenshot' />
-          <div className={styles.projectConnections}>
-            <img className={styles.faIcon} src='/images/connectdevelop-brands.svg' alt='See it live' />
-            <img className={styles.gitHubMark} src='/images/GitHub-Mark-64px.png' alt='See it on GitHub' />
-            <img className={styles.youTubeIcon} src='/images/youtube_social_square_red.png' alt='See demo on YouTube' />
-          </div>
-        </div>
-        <div className={styles.portfolioItem}>
-          <h3>Project Title</h3>
-          <img src='https://picsum.photos/200?random=5' alt='Project Screenshot' />
-          <div className={styles.projectConnections}>
-            <img className={styles.faIcon} src='/images/connectdevelop-brands.svg' alt='See it live' />
-            <img className={styles.gitHubMark} src='/images/GitHub-Mark-64px.png' alt='See it on GitHub' />
-            <img className={styles.youTubeIcon} src='/images/youtube_social_square_red.png' alt='See demo on YouTube' />
-          </div>
-        </div>
-        <div className={styles.portfolioItem}>
-          <h3>Project Title</h3>
-          <img src='https://picsum.photos/200?random=6' alt='Project Screenshot' />
-          <div className={styles.projectConnections}>
-            <img className={styles.faIcon} src='/images/connectdevelop-brands.svg' alt='See it live' />
-            <img className={styles.gitHubMark} src='/images/GitHub-Mark-64px.png' alt='See it on GitHub' />
-            <img className={styles.youTubeIcon} src='/images/youtube_social_square_red.png' alt='See demo on YouTube' />
-          </div>
-        </div>
       </div>
     </div>
   )
