4056fe11cc0f73fc5429c71553742fbea84ff27e
steven <sfriggs21@gmail.com>
2025-08-14 15:14:05 -0600

    changed profile image display for Welcome screen

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -22,28 +22,30 @@ const Welcome = () => {
 
   return (
     <>
-      <div id='home' className={`bgDark ${styles.welcomeDiv}`}>
-        <h1>Hello,<br />I&apos;m <span onMouseEnter={toggleProfilePic} onMouseLeave={toggleProfilePic}>Steven</span></h1>
+      <div class={`bgDark ${styles.flexContainer}`}>
+        <div className={styles.imageDiv}>
+          <Image src={'/images/Steven_Riggs_photo_cropped.png'} alt='Steven Riggs' width={860} height={860} />
+        </div>
 
-        <p>
-          A <a href='https://www.w3schools.com/whatis/whatis_fullstack.asp' target='_blank' rel='noreferrer' onMouseEnter={togglePopupDef} onMouseLeave={togglePopupDef}>Full Stack Developer</a><br /><a className='hiddenLink' href='#about'>Here for Your Needs</a>
-        </p>
-      </div>
+        <div id='home' className={styles.welcomeDiv}>
+          <h1>Hello,<br />I&apos;m <span onMouseEnter={toggleProfilePic} onMouseLeave={toggleProfilePic}>Steven</span></h1>
 
-      <div className={styles.imageDiv} hidden={hideProfilePic}>
-        <Image src={'/images/Steven_Riggs_photo.jpg'} alt='Steven Riggs' width={1008} height={1344} />
-      </div>
+          <p>
+            A <a href='https://www.w3schools.com/whatis/whatis_fullstack.asp' target='_blank' rel='noreferrer' onMouseEnter={togglePopupDef} onMouseLeave={togglePopupDef}>Full Stack Developer</a><br /><a className='hiddenLink' href='#about'>Here for Your Needs</a>
+          </p>
+        </div>
 
-      <div className={styles.popupDef} hidden={hidePopupDef}>
-        <q cite='https://www.w3schools.com/whatis/whatis_fullstack.asp'>
-          A full stack web developer is a person who can develop both client and server software.
-        </q>
+        <div className={styles.popupDef} hidden={hidePopupDef}>
+          <q cite='https://www.w3schools.com/whatis/whatis_fullstack.asp'>
+            A full stack web developer is a person who can develop both client and server software.
+          </q>
 
-        <br /><br />
+          <br /><br />
 
-        <span>
-          &nbsp;&nbsp;&nbsp;-- W3 Schools
-        </span>
+          <span>
+            &nbsp;&nbsp;&nbsp;-- W3 Schools
+          </span>
+        </div>
       </div>
     </>
   )
