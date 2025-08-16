581b6ff3267ec160489052f924914cc2ac4245fd
steven <sfriggs21@gmail.com>
2025-08-16 13:04:05 -0600

    reodered imports

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import striptags from 'striptags'
